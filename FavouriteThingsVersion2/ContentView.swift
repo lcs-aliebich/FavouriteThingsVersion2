536a6a81e731c6043c95e00bea2651fe2f47daf5
Andreas Liebich <aliebich@lcs.on.ca>
2020-09-17 09:12:56 -0400

    Initial Commit

---
+++ FavouriteThingsVersion2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteThingsVersion2
+//
+//  Created by Andreas Liebich on 2020-09-17.
+//  Copyright © 2020 Andreas Liebich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
